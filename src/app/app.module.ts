8a1336f9112429a36dc28973a7796eab567cb216
Benjalorc <benjalorc@gmail.com>
2018-04-13 16:02:18 -0400

    - Carga de componentes Inicio y Dashboard - Carga de servicio Gapi - Configuración de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import { GapiService } from './services/gapi/gapi.service';
+
+const appRoutes : Routes = [
+  { path: '', component: InicioComponent},
+  { path: 'dashboard', component: DashboardComponent}
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [GapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
