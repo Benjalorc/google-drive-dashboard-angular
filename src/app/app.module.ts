214e235a1c3f0c65ceb69d1987e9d7462d09d262
Benjalorc <benjalorc@gmail.com>
2018-04-13 20:39:54 -0400

    Se incluyeron los componentes de Bootstrap y FontAwesome para implementacion en el diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -22,6 +24,7 @@ const appRoutes : Routes = [
   ],
   imports: [
     BrowserModule,
+    AngularFontAwesomeModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [GapiService],
