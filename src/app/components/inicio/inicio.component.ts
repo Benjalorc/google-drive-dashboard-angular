e709292400b93e2a9a7ee8c23a8a5e5de49b69e2
Benjalorc <benjalorc@gmail.com>
2018-04-15 10:25:12 -0400

    ---Adaptacion de estilos y mejoras de UI
    Se incluyó un componente carousel con 3 imagenes genericas de prueba.
    
    Modificada la ubicacion del boton de inicio de sesion.
    
    Ajustadas las dimensiones y ubicacion de foto de perfil y tarjeta de bienvenida.
    
    Elementos de usabilidad.

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inicio',
@@ -15,16 +16,22 @@ export class InicioComponent implements OnInit {
 	imgUrl: string;
 
 	loading: boolean;
+	showSwitch: boolean;
+
+	images: string[] = ["https://picsum.photos/1000/450/?image=307", "https://picsum.photos/1000/450/?image=509", "https://picsum.photos/1000/450/?image=109"];
 
   constructor(private router: Router, 
   			  private route:ActivatedRoute,
-  			  private cd: ChangeDetectorRef) { 
+  			  private cd: ChangeDetectorRef,
+  			  config: NgbCarouselConfig) { 
+	
+		config.interval = 7000;
 
-      let _self = this;
-      _self.gapi = window["gapi"];
-      setTimeout(() =>{
-	    _self.renderButton();
-      },200);
+		let _self = this;
+		_self.gapi = window["gapi"];
+		setTimeout(() =>{
+			_self.renderButton();
+		},200);
   }
 
   ngOnInit() {
@@ -57,10 +64,10 @@ export class InicioComponent implements OnInit {
   	
 	  	this.gapi.signin2.render('my-signin2', {
 	        'scope': 'profile email',
-	        'width': 240,
+	        'width': 220,
 	        'height': 50,
 	        'longtitle': true,
-	        'theme': 'dark',
+	        'theme': 'light',
 	        'onsuccess': onSignIn,
 	        'onfailure': onFailure
 	    });
@@ -78,5 +85,12 @@ export class InicioComponent implements OnInit {
 	cambiarUsuario(){
 		let element: HTMLElement = document.querySelector("#my-signin2 > div") as HTMLElement;
 		element.click()
+	}
+
+	toggleSwitch(e){
+
+		if(e=='on') this.showSwitch = true;
+		if(e=='off') this.showSwitch = false;
+		this.cd.detectChanges();
 	}
 }
